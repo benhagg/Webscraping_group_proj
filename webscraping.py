8c746f26146b4b7b5d70dd104fcfe4e02ed64729
benhagg <21bhaggard@gmail.com>
2024-04-12 11:00:36 -0600

    part 2 logical flow

--- webscraping.py
+++ webscraping.py
@@ -10,12 +10,15 @@ import sqlalchemy
 
 
 # Part 2 _________________________________________________________________________________________________________
-
-def part_2_1():
+query = 'SELECT * FROM general_conference'
+df = pd.read_sql(query, engine)
+def chart_all_talks():
+    # matplotlib function to plot all talks
     pass
 
 
-def part_2_2():
+def chart_individual_talk(talk_id):
+    # matplotlib function to plot individual talk
     pass
 
 
@@ -39,6 +42,16 @@ def part_2_2():
 if __name__ == '__main__':
     user_input = int(input("Enter 1 for Part 1 or 2 for Part 2: ")) # change the wording
     if user_input == 2:
-        print('You selected to see summaries \nPress 1 for summaries of all talks \nPress 2 for summaries of an indibidual talk')
-        print('Press anything else to exit')
-        part_2_1()
+        # 1
+        user_input2 = input('You selected to see summaries \nPress 1 for summaries of all talks \nPress 2 for summaries of an indibidual talk \nPress anything else to exit')
+        # 2
+        if user_input2 == 1:
+            chart_all_talks()
+        if user_input2 ==  2:
+            print('The following are the names of speakers and their talks')
+            for row in df.iterrows():
+                print(f"{row['id']}: {row['speaker']} - {row['title']}")
+        # 4
+            user_input3 = input('Enter the number of the talk you want to see the summary of: ')
+        # 5
+            chart_individual_talk(user_input3)
