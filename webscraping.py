0872f1a777511c53e93e3dff71f49b6f3d116a40
benhagg <21bhaggard@gmail.com>
2024-04-11 18:37:01 -0600

    imports

---
+++ webscraping.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import sqlalchemy
+
+# Part 1 _________________________________________________________________________________________________________
+
+
+
+
+# Part 2 _________________________________________________________________________________________________________
+
+def part_2_1():
+    pass
+
+
+def part_2_2():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    user_input = int(input("Enter 1 for Part 1 or 2 for Part 2: ")) # change the wording
+    if user_input == 2:
+        print('You selected to see summaries \nPress 1 for summaries of all talks \nPress 2 for summaries of an indibidual talk')
+        print('Press anything else to exit')
+        part_2_1()
