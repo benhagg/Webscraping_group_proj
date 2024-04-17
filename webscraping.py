c7436142ccc164dadb7b7dd970b2584de16be01e
benhagg <153319291+benhagg@users.noreply.github.com>
2024-04-17 14:29:14 -0600

    final commit fr
    had to fix some issues with a pull request from a seperate commit

--- webscraping.py
+++ webscraping.py
@@ -1,3 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import sqlalchemy
+import re
+
+
+
 # General Conference Webscraping
 # For this project you are going to web scrape the talks from a recent
 # general conference of The Church of Jesus Christ of Latter-day Saints which you can find here.
@@ -9,13 +18,7 @@
 
 # Group Members: Ryan Briggs, Joshua Gillespie, and Ben Haggard MW 12:30 - 1:45 class
 
-# importing required libraries
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-import sqlalchemy
-from sqlalchemy import sql
-import matplotlib.pyplot as plt
+# Part 1 _________________________________________________________________________________________________________
 
 # Create a connection to the database
 database_name = "is303"
@@ -26,49 +29,8 @@ db_port = "5432" # default setting
 
 engine = sqlalchemy.create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{database_name}')
 
-# fake data entry TESTING PURPOSES -----------------------------------------------------------------------------------------------
-# fake_data = {
-#     'Speaker_Name': ['John Smith', 'Emily Johnson', 'David Williams', 'Sarah Brown', 'Michael Davis'],
-#     'Talk_Name': ['Faith and Doubt', 'Hope in Adversity', 'Love and Forgiveness', 'Service and Sacrifice', 'Courage and Resilience'],
-#     'Kicker': ['Inspiring Message', 'Powerful Reminder', 'Uplifting Story', 'Heartfelt Testimony', 'Motivational Speech'],
-#     'Matthew': [1, 5, 2, 6, 0],
-#     'Mark': [6, 8, 4, 7, 0],
-#     'Luke': [0, 7, 0, 6, 1],
-#     'John': [5, 6, 7, 8, 0],
-#     'Acts': [1, 0, 0, 0, 1],
-#     # Add fake data for the rest of the books...
-#     'Articles of Faith': [0, 1, 0, 0, 0]
-# }
 
-# df = pd.DataFrame(fake_data)
-# insert_data = df.to_sql("general_conference", engine, if_exists='replace', index=False) # reason to make it into a dataframe
-# conn = engine.connect()
-# conn.commit()
-# conn.close()
-# this is fake data for testing purposes delete before turning in
-# END fake data entry TESTING PURPOSES -----------------------------------------------------------------------------------------------
-
-# Part II Functions: 
-def chart_all_talks(df):
-    # filtered_dict = {key: value for key, value in dict.items() if value > 2}
-    sum_val_df = df.drop(['Speaker_Name', 'Talk_Name', 'Kicker'], axis=1).sum()
-    sum_val_df = sum_val_df[sum_val_df > 2] # filter out values less than 2
-    plt.bar(sum_val_df.index, sum_val_df)
-    plt.title('Standard Works Referenced in General Conference')
-    plt.ylabel('Number of References')
-    plt.show()
-
-def chart_individual_talk(talk_id, df):
-    talk_name = df.iloc[int(talk_id) - 1]['Talk_Name'] # gets the title value of the talk_id
-    std_works = df.iloc[int(talk_id) - 1].drop(['Speaker_Name', 'Talk_Name', 'Kicker']) # drops the speaker name, talk name, and kicker
-    std_works = std_works[std_works >= 1] # filter out values less than 2
-    plt.bar(std_works.index, std_works) # (talk: x, talk row: y)
-    plt.title(f'Standard Works Referenced in {talk_name}')
-    plt.xlabel('Standard Works Books')
-    plt.ylabel(f'Number of References in {talk_name}')
-    return plt
-
-# Dictionary
+# DICTIONARY
 conference_dictionary = {"Speaker Name" : [], "Talk Name" : [], "Kicker" : []} # do we use this? - Ben
 standard_works_dict = {'Speaker_Name' : [], 'Talk_Name' : [], 'Kicker' : [],
 'Matthew': [], 'Mark': [], 'Luke': [], 'John': [], 'Acts': [], 'Romans': [], 
@@ -90,70 +52,114 @@ standard_works_dict = {'Speaker_Name' : [], 'Talk_Name' : [], 'Kicker' : [],
 [], 'Abraham': [], 'Joseph Smith—Matthew': [], 'Joseph Smith—History': [],
 'Articles of Faith': []}
 
+# # set all books to zero
+# standard_works_dict = {key: 0 for idx, key in enumerate(standard_works_dict)}
+# for key in list(standard_works_dict.keys())[3:]:
+#     standard_works_dict[key] = 0
+
+# part 2 functions -----------------------------------------------------------------------------------------------
+def chart_all_talks(df):
+    # filtered_dict = {key: value for key, value in dict.items() if value > 2}
+    sum_val_df = df.drop(['Speaker_Name', 'Talk_Name', 'Kicker'], axis=1).sum()
+    sum_val_df = sum_val_df[sum_val_df > 2] # filter out values less than 2
+    plt.bar(sum_val_df.index, sum_val_df)
+    plt.title('Standard Works Referenced in General Conference')
+    plt.ylabel('Number of References')
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+def chart_individual_talk(talk_id, df):
+    talk_name = df.iloc[int(talk_id) - 1]['Talk_Name'] # gets the title value of the talk_id
+    std_works = df.iloc[int(talk_id) - 1].drop(['Speaker_Name', 'Talk_Name', 'Kicker']) # drops the speaker name, talk name, and kicker
+    std_works = std_works[std_works >= 1] # filter out values less than 2
+    plt.bar(std_works.index, std_works) # (talk: x, talk row: y)
+    plt.title(f'Standard Works Referenced in {talk_name}')
+    plt.xlabel('Standard Works Books')
+    plt.ylabel(f'Number of References in {talk_name}')
+    plt.xticks(rotation=90)
+    return plt
+
+# PART I steps 4-6 -----------------------------------------------------------------------------------------------
 
 user_input = input("If you want to scrape data, enter 1. If you want to see summaries of stored data, enter 2. Enter any other value to exit the program: ")
 
 if user_input == "1":
-    # Part I
-    # Step 1 (dropping sql query if exists)
-    drop_table_query = sqlalchemy.text("drop table if exists general_conference;")
+    # PART I steps 1-2 (dropping sql query if exists) PUT INTO LOOP
+    drop_sql_query = sqlalchemy.text("drop table if exists general_conference;")
     conn = engine.connect()
-    conn.execute(drop_table_query)
+    conn.execute(drop_sql_query)
     conn.commit()
     conn.close()
 
-    # Step 2
-    # Creating requests and soup variables to get access to website
-    response = requests.get("https://www.churchofjesuschrist.org/study/general-conference/2023/10?lang=eng ")
+    # creating requests and soup variables to get access to website
+    response = requests.get("https://www.churchofjesuschrist.org/study/general-conference/2023/10?lang=eng")
     soup = BeautifulSoup(response.content, "html.parser")
-    talk_links = soup.find_all("li") # gets all <a> tags
+    # check for response status
+    soup = soup.find('ul', attrs= {'class' : 'doc-map'})
+    talk_links = soup.find_all("a") # gets all <a> tags
     hrefs = [link.get("href") for link in talk_links]
 
-    # Step 3
-    # Create talk list and filter data
+    # get url for each talk
     talk_list = []
-    for talks in talk_links:
-        gc_talks = talks.get("href")
-        if "Session" not in gc_talks:
-            talk_list.append('https://www.churchofjesuschrist.org' + gc_talks)
+    for talk in hrefs:
+        # gc_talks = talks.get("href")
+        if "session" not in talk:
+            talk_list.append('https://www.churchofjesuschrist.org' + talk)
             
 
     for gc_talks in talk_list:
-        # Runs through requests every time the loop iterates
+        # runs through requests every time the loop iterates
         print(f"Trying to scrape url: {gc_talks}")
         response = requests.get(gc_talks)
         specific_talk = BeautifulSoup(response.content, "html.parser") # parses the specific talks and setting up for eliminating session and sustaining
-        print(gc_talks)
-        # this is where if statement for preventing "sustaining" will go
-        if "Sustain" in gc_talks:
-            continue
 
 
         # REST OF CODE IS IN THE FOR LOOP
 
         # speakers name
         name_element = specific_talk.find("p", attrs= {"class" : "author-name"}).get_text()
-        standard_works_dict["Speaker Name"].append(name_element.text.strip())
-        print(name_element)
+        if name_element.startswith("Presented by"): # remove the name element from the sustaining page
+            continue
+        else:
+            name_element.replace('\xa0', ' ')
+            if name_element.startswith("By "):
+                name_element = name_element[3:]
+            standard_works_dict["Speaker_Name"].append(name_element)
+            print(name_element)
 
         # talk name (same formula as above)
         talk_name_element = specific_talk.find("h1", attrs= {"id" : "title1"}).get_text()
-        standard_works_dict["Talk Name"].append(talk_name_element.text.strip()[3:])
-        print(talk_name_element)
+        if "Sustaining of General Authorities, Area Seventies, and General Officers" == talk_name_element:
+            continue # removing the sustainaing of general authorities
+        standard_works_dict["Talk_Name"].append(talk_name_element)
 
         # kicker (same formula as above)
         kicker_element = specific_talk.find("p", attrs= {"id" : "kicker1"}).get_text()
-        standard_works_dict["Kicker"].append(kicker_element.text.strip())
-        print(kicker_element)
+        standard_works_dict["Kicker"].append(kicker_element)
 
         # counting references
         footnote_section = specific_talk.find('footer', attrs= {'class' : 'notes'})
         if footnote_section is not None:
-            footnote_section.count(specific_talk.find('footer', attrs= {'class' : 'notes'}))
+            std_books = footnote_section.find_all('a')  # find all links in the footer 
+            std_books = [book.get_text() for book in std_books] # get all the hyperlink rferences visible text
+            std_books = [book.replace('\xa0', ' ') for book in std_books] # replace the unicode with a space
+            for key in standard_works_dict:
+                book_count = 0
+                for book in std_books:
+                    if book.startswith(key):
+                        book_count += 1
+                if key in ['Talk_Name', 'Speaker_Name', 'Kicker']:
+                    continue
+                else:
+                    standard_works_dict[key].append(book_count)
         else:
-            continue
-            
-    # Exports to database
+            for key in standard_works_dict:
+                if key in ['Talk_Name', 'Speaker_Name', 'Kicker']:
+                    continue
+                else:
+                    standard_works_dict[key].append(0)
+
     general_conference_df = pd.DataFrame(standard_works_dict)
     general_conference_df.to_sql("general_conference", engine, if_exists= 'replace', index= False) # reason to make it into a dataframe
 
@@ -176,7 +182,9 @@ if user_input == "1":
     print("You've saved the scraped data to your postgres database.")
 
 
-# Part II
+
+# Part 2 _________________________________________________________________________________________________________
+
 
 elif user_input == "2":
 
@@ -199,7 +207,4 @@ elif user_input == "2":
             user_input3 = input('\nEnter the number of the talk you want to see the summary of: ')
         # 5
             chart_individual_talk(user_input3, output_df).show()
-        
-else:
-    print("Exiting Program")
-    exit()
+
